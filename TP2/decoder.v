9a8a97b0aaf921a450073d505dd5bcd55ad61bac
jucardozo <jucardozo@itba.edu.ar>
2023-05-19 19:33:38 -0300

    decoder basico funcinado ,testeado. Falta ver temas de branchs yel Control Trasnfer Instruction

--- TP2/decoder.v
+++ TP2/decoder.v
@@ -1,27 +1,88 @@
-module decoder
-(
- input[7:0] opcode,
- output reg [1:0] selA,
- output reg [1:0] selB,
- output reg [3:0] alu, 
- output reg save0,
- output reg save1,
- output reg save2
- );
+//PRIMERA IMPLEMENTACION DECODER PARA RISC V 32I
+// DECODER
+module Decoder ( instruccion ,A,B,C,ALU,imm_out);	//PEnsar como afecta si es litle/big endian
 
-	reg[7:0] q;
-	 
-	always @(opcode)
-	begin
-		case (opcode)
-			0:  begin selA=0; selB=0; alu=4'b0000; save0=0; save1=0; save2=0; end //NOP
-			1:  begin selA=0; selB=1; alu=4'b0010; save0=0; save1=0; save2=1; end //REG2 = REG0 + REG1
-			2:  begin selA=0; selB=1; alu=4'b0011; save0=0; save1=0; save2=1; end //REG2 = REG0 - REG1
-			3:  begin selA=0; selB=0; alu=4'b0100; save0=1; save1=0; save2=0; end //REG0 = REG0 + 1
-			4:  begin selA=1; selB=0; alu=4'b0100; save0=0; save1=1; save2=0; end //REG1 = REG1 + 1
-		endcase 
-	end
-
-	assign nextPC = q;
+ input[31:0] instruccion;
+ //input wire clk,  //clk ver bien como se referenciaba esa entrada
+ output reg [4:0] A;//rs1;
+ output reg [4:0] B; //rs2
+ output reg [4:0] C; //rd
+ output reg [2:0] ALU; //control de la alu
+ output reg [11:0] imm_out;	// IMM,a chequear ... no se bien cuantos bits tienen que salir
+ always @(instruccion) // creo que aca tendria que poner el flanco de algun clock
  
+ begin 
+	case (instruccion[6:0]) //falta  nop => addi rs1=0 y rd=0
+		//Memory access , LOAD
+		7'b0000011: begin
+							 A=instruccion[19:15];		//entre A y immout forman el efective address =>17bits,lugar a donde ir a buscar un dato
+							 imm_out=instruccion[31:20];
+							 C=instruccion[11:7];			//regitro al que se le carga un dato de la memoria
+							 ALU=instruccion[14:12];		//ojo que esto no iria a la alu, xq indica el ancho de la palabra
+							 
+							end	// de memo carga en rd
+		
+		//Interger RegisterImmediate Instructinon
+		7'b0010011: begin  							
+							 A=instruccion[19:15];
+							 C=instruccion[11:7];		
+							 ALU=instruccion[14:12];
+							 imm_out=instruccion[31:20];
+							end  	
+				  
+		//Memory Access , STORE
+		7'b0100011: begin 
+							 C=instruccion[11:7];	//usados para indicar el efective address =>17bits, lugar a donde ir a guardar
+							 A=instruccion[19:15];		
+							 imm_out=instruccion[31:25];
+							 B=instruccion[24:20];	 	//registro del cual se va a guarda el dato que posee en memoria
+							 ALU=instruccion[14:12];		//usados pa distinguir el largo del dato
+							 
+							end	//de rs2=> memo		
+		
+		//Integer Register-Register Operations
+		7'b0110011: begin //creo que no hace falta
+							 A=instruccion[19:15];
+							 B=instruccion[24:20];
+							 C=instruccion[11:7];
+							 ALU=instruccion[14:12];		
+							 imm_out=instruccion[31:25];
+							end 
+		default: begin				//en caso de que pase algo , mando una instruccion NOP
+						C=5'b00000;
+						ALU=3'b000;
+						A=5'b00000;
+						imm_out=12'b000000000000;
+					end
+	endcase
+end
+	
 endmodule
+
+
+
+/*TESTBENCH*/
+/*`timescale 1ns/1ns
+
+
+module decoder_testbench;		
+
+ reg [31:0] instruccion;
+ reg [4:0] A;//rs1;
+ reg [4:0] B; //rs2
+ reg [4:0] C; //rd
+ reg [2:0] ALU; //control de la alu
+ reg [11:0] imm_out;	// IMM,a chequear
+ 
+ Decoder DUT(.instruccion(instruccion), .A(A) ,.B(B) ,.C(C) ,.ALU(ALU) ,.imm_out(imm_out));	//instancio mi unidad
+ 
+ initial
+	begin
+		instruccion = 32'b00000000000100010000000110110011;//x3=x1+x2 //rs1=00001 rs2=00010 rd=00011
+		#5ns;
+		instruccion = 32'b01000000000100010000000110110011; //x3=x1-x2
+		#5ns ; 
+		$stop;
+	end
+endmodule*/
+	
